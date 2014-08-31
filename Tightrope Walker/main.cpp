9e110bf413037b2982b8cb01c008eb765eccb063
marcelozeballos <zeballosmarcelo@icloud.com>
2014-08-31 13:00:14 -0400

    Unsolved

---
+++ Tightrope Walker/main.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int len;
+    while(scanf("%d", &len) != EOF) // leer hasta fin de archivo
+    {
+        string inst; cin >> inst; // cadena que contiene las instrucciones
+        float current, threshold;
+        bool caida = false; // bool que controla si el tipo se cayo o no. comienza en falso para indicar que no hay caida
+        scanf("%f %f", &current, &threshold);
+
+        for(int i = 0; i < len; i++) // este for recorre uno a uno los caracteres de la cadena
+        {
+            if(inst[i] == 'L') // si es l, el angulo baja
+                current--;
+            if(inst[i] == 'R') // si es r el angulo sube
+                current++;
+            if(current >= threshold)// si current (rl angulo de ahora) fuera igual o mayor a threshold (el limite) el
+            {
+                caida = true; //tipo se cae y el for se rompe
+                break;
+            }
+        }
+        if(!caida)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+
+    return 0;
+}
